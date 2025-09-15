4d71d30cac1c520a27035780e65f94fb30075897
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-09-15 11:29:44 -0400

    type checking declerations

--- Compiler/Frontend/typechecker.hpp
+++ Compiler/Frontend/typechecker.hpp
@@ -36,8 +36,34 @@ namespace Frontend {
         return DS::View<char>();
     }
 
+    DS::View<char> type_check_decleration(Decleration* decl) {
+        switch (decl->type) {
+            case DECLERATION_TYPE_VARIABLE: {
+                DS::View<char> expression_type_name = type_check_expression(decl->variable->right);
+                RUNTIME_ASSERT_MSG(String::equal(decl->variable->type_name, expression_type_name), "Decleration left and right are not the same!\n");
+
+                return decl->variable->type_name;
+            } break;
+
+            default: {
+                RUNTIME_ASSERT(false);
+            } break;
+        }
+
+        RUNTIME_ASSERT(false);
+        
+        return DS::View<char>();
+    }
+
     void type_check_ast(ASTNode* node) {
         switch (node->type) {
+            case AST_NODE_PROGRAM: {
+                Program* p = node->program;
+                for (Decleration* decl : p->declerations) {
+                    type_check_decleration(decl);
+                }
+            } break;
+
             case AST_NODE_EXPRESSION: {
                 Expression* e = node->expression;
                 type_check_expression(e);
